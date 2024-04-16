0d8319105c93510cbbc400e94e88b9ebb6d2d8ac
kruse <krusezh@gmail.com>
2024-04-17 00:08:59 +0800

    fix(keymap): key <TAB> conflict
    change copilot accept from <TAB> to <RIGHT>

--- lua/plugin-config/copilot.lua
+++ lua/plugin-config/copilot.lua
@@ -19,7 +19,7 @@ require("copilot").setup({
 		auto_trigger = true,
 		debounce = 75,
 		keymap = {
-			accept = "<TAB>",
+			accept = "<RIGHT>",
 			accept_word = false,
 			accept_line = false,
 			next = "<C-[>",
